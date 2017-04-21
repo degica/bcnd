1b93e2b42f4ce806d3f49005a7a1070fcdc25646
Kazunori Kajihiro <kazunori.kajihiro@gmail.com>
2017-04-21 17:54:54 +0900

    Add spec to test when a docker tag exists

--- spec/bcnd/runner_spec.rb
+++ spec/bcnd/runner_spec.rb
@@ -66,6 +66,33 @@ describe Bcnd::Runner do
         expect(stub3).to have_been_requested
         expect(stub4).to have_been_requested
       end
+
+      context "when the tag already exists" do
+        it "skips tagging" do
+          stub = stub_request(:get,  "https://quay.io/api/v1/repository/org/repo/tag/")
+                   .with(query: hash_including("specificTag" => "aaaaaa"))
+                   .to_return(
+                     body: {
+                       tags: [
+                         {
+                           "reversion" => false,
+                           "manifest_digest" => "sha256:97b6de52ce9d9d89766144e0a9a0fe4a151741ecd812884ce804f41bdb672419",
+                           "start_ts" => 1492592525,
+                           "name" => "aaaaaa",
+                           "docker_image_id" => "aacf174aa6cf1a9de0efb5e6164cbc2556965fa469e94725c0de596e1e01a2d1"
+                         },
+                       ]
+                     }.to_json
+                   )
+
+          runner = described_class.new
+          expect(runner).to receive(:system).with("bcn deploy -e staging --tag aaaaaa --heritage-token mainline_heritage_token 1> /dev/null") do
+            system 'true'
+          end
+          expect{runner.deploy}.to_not raise_error
+          expect(stub).to have_been_requested
+        end
+      end
     end
 
     context "when production branch" do
